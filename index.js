b1c91eb0ffd5bd2e6a048794476f8c8add6d9555
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-03-01 22:56:21 +0530

    created by manoj

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const authorName = document.querySelector(".author_name");
 quoteBtn = document.querySelector("button");
 soundBtn = document.querySelector(".sound");
 copyBtn = document.querySelector(".copy");
-console.log(copyBtn);
-console.log(soundBtn);
+
 function randomQuote() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading Quote ...";
-  console.log("clicked");
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       quoteText.innerText = data.content;
       authorName.innerText = data.author;
       quoteBtn.innerText = "New Quote";
@@ -25,7 +22,6 @@ soundBtn.addEventListener("click", () => {
   try {
     console.log("click");
     let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText}`);
-    console.log(utterance);
     speechSynthesis.speak(utterance);
   } catch (e) {
     console.log(e);
